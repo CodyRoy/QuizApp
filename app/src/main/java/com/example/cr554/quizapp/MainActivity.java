b33d7526aca899a2f57fc76416ba9927ad5a1a7e
cr554 <cody.roy@yale.edu>
2017-02-10 12:12:26 -0500

    Added Submit button functionality:
    Quiz's are now stored in an array. Submit button iterates a counter then displays next question in array

--- app/src/main/java/com/example/cr554/quizapp/MainActivity.java
+++ app/src/main/java/com/example/cr554/quizapp/MainActivity.java
@@ -4,6 +4,7 @@ import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.RadioButton;
 import android.widget.TextView;
 
@@ -11,24 +12,34 @@ import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
 
+    private int questionCounter =0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         //probably terrible performance in large enough quantities
+
         Quiz quiz1 = new Quiz(getString(R.string.question_one), getString(R.string.q1_ans1), getString(R.string.q1_ans2), getString(R.string.q1_ans3));
         Quiz quiz2 = new Quiz(getString(R.string.question_two), getString(R.string.q2_ans1), getString(R.string.q2_ans2), getString(R.string.q2_ans3));
         Quiz quiz3 = new Quiz(getString(R.string.question_three), getString(R.string.q3_ans1), getString(R.string.q3_ans2), getString(R.string.q3_ans3));
+        final Quiz[] fullQuiz = new Quiz[]{quiz1,quiz2,quiz3};
 
-        quizPopulate(quiz1);
-        //populate layout with quiz stuff
-        //textview = question
-        //radiobuttonn1-3 = questions. Randomly populate? Probably way past scope.
-    }
 
-    public void submit(View view){
-        //submit the question(s) for grading
-        //<advance to next q>
+        quizPopulate(fullQuiz[questionCounter]);
+
+        final Button button = (Button) findViewById(R.id.submitButton);
+        button.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View view){
+                if (questionCounter == fullQuiz.length){
+                    //things
+                }
+                else{
+                    //right/wrong
+                    questionCounter++;
+                    quizPopulate(fullQuiz[questionCounter]);
+                }
+            }
+        });
     }
 
     public void quizPopulate(Quiz quiz){
